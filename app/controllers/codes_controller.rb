7742b2062c954f5e4807f7d3b72aa61097414d72
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-08-05 19:10:19 +0900

    Add post-view & test jquery

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,13 +1,23 @@
 class CodesController < ApplicationController
-
+before_action :authenticate_user!
 	def index
-		@user = User.find(1)
+		@user = current_user
+		@code = Code.new
+		@codes = Code.all
+		@users = User.all
 	end
 
 	def new
 	end
 
 	def create
+		@code = Code.new(code_params)
+		@code.user_id == current_user.id
+		if @code.save
+           redirect_to codes_path
+        else
+           redirect_to codes_path
+        end
 	end
 
 	def show
@@ -20,6 +30,12 @@ class CodesController < ApplicationController
 	end
 
 	def destroy
+	end
+
+	private
+
+	def code_params
+		params.require(:code).permit(:post_image, :category, :user_id, :text)
 	end
 
 end
